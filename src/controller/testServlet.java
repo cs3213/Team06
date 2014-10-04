f73cee918ac6293994e1f8deda4a19cde0b1f297
KevinChenHao <westernwolf103@gmail.com>
2014-10-04 15:00:50 +0800

    commit

--- src/controller/testServlet.java
+++ src/controller/testServlet.java
@@ -2,12 +2,13 @@ package controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
- 
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
  
+@SuppressWarnings("serial")
 public class testServlet extends HttpServlet {
      
  
